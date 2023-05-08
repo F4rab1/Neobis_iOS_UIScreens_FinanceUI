7da0c263b15ad9fd6092804432519808f0d21ca1
F4rab1 <issafarabi2022@gmail.com>
2023-05-09 00:37:21 +0600

    Initial Commit

---
+++ FinanceUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceUI
+//
+//  Created by Фараби Иса on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
