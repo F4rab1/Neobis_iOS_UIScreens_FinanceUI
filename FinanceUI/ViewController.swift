cf465975810b93cce714834cd09f31421a22d8b8
F4rab1 <issafarabi2022@gmail.com>
2023-05-09 13:58:08 +0600

    Added tableview

--- FinanceUI/ViewController.swift
+++ FinanceUI/ViewController.swift
@@ -68,9 +68,27 @@ class ViewController: UIViewController {
         return button
     }()
     
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.isScrollEnabled = false
+        tableView.isUserInteractionEnabled = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let cellReuseIdentifier = "cell"
+    let images = ["house", "basket", "face.smiling", "heart", "figure.run", "envelope", "heart"]
+    let names = ["Дом", "Покупки", "Транспорт", "Здоровье", "Фитнес", "Счета", "Ресторан"]
+    let priceLabel2 = ["$321", "$574", "$124", "$765", "$324", "$726", "$325"]
+    let descryption = ["Продукты", "Одежда", "Такси", "Лечение", "Тренировки", "Комунальные", "Ресторан"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
         
         setupViews()
         setupConstraints()
@@ -86,6 +104,7 @@ class ViewController: UIViewController {
         view.addSubview(leftButton)
         view.addSubview(rightButton)
         view.addSubview(background)
+        view.addSubview(tableView)
         background.addSubview(alsoButton)
     }
     
@@ -104,7 +123,7 @@ class ViewController: UIViewController {
         tabBarController.viewControllers = [firstViewController, secondViewController, thirdViewController]
         
         let tabBar = tabBarController.tabBar
-        tabBar.tintColor = .systemYellow
+        tabBar.tintColor = .black
         
         let topDividerView = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.width, height: 1))
         topDividerView.backgroundColor = .lightGray
@@ -140,14 +159,109 @@ class ViewController: UIViewController {
             background.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -60),
             
             alsoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            alsoButton.bottomAnchor.constraint(equalTo: background.bottomAnchor, constant: -20)
+            alsoButton.bottomAnchor.constraint(equalTo: background.bottomAnchor, constant: -20),
+            
+            tableView.topAnchor.constraint(equalTo: background.topAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: background.leadingAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: background.trailingAnchor, constant: -10),
+            tableView.bottomAnchor.constraint(equalTo: alsoButton.topAnchor, constant: -20)
         ])
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+        
+        let stackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .horizontal
+            stackView.alignment = .center
+            stackView.spacing = 10
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            return stackView
+        }()
+        
+        let name: UILabel = {
+            let label = UILabel()
+            label.text = names[indexPath.row]
+            label.textColor = .black
+            return label
+        }()
+        
+        let image: UIButton = {
+            let button = UIButton()
+            switch indexPath.row {
+            case 0:
+                button.backgroundColor = .systemYellow
+            case 1:
+                button.backgroundColor = .purple
+            case 2:
+                button.backgroundColor = .green
+            case 3, 6:
+                button.backgroundColor = .systemPink
+            default:
+                button.backgroundColor = .blue
+            }
+            button.layer.cornerRadius = 15
+            button.setImage(UIImage(systemName: images[indexPath.row]), for: .normal)
+            button.tintColor = .white
+            button.widthAnchor.constraint(equalToConstant: 30).isActive = true
+            button.heightAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+            return button
+        }()
+
+        let rightStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.alignment = .trailing
+            return stackView
+        }()
+
+        let priceLabel: UILabel = {
+            let label = UILabel()
+            label.text = priceLabel2[indexPath.row]
+            label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+            return label
+        }()
+        rightStackView.addArrangedSubview(priceLabel)
+
+        let descryptionOf: UILabel = {
+            let label = UILabel()
+            label.text = descryption[indexPath.row]
+            label.textColor = .lightGray
+            label.font = UIFont.systemFont(ofSize: 11)
+            return label
+        }()
+        rightStackView.addArrangedSubview(descryptionOf)
+
+        stackView.addArrangedSubview(image)
+        stackView.addArrangedSubview(name)
+        stackView.addArrangedSubview(rightStackView)
+        
+        cell.contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: cell.contentView.topAnchor, constant: 13),
+            stackView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -10),
+            stackView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: -13)
+        ])
+        
+        return cell
     }
 }
 
 extension ViewController: UITabBarControllerDelegate {
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        tabBarController.tabBar.tintColor = .systemYellow
+        tabBarController.tabBar.tintColor = .black
     }
 }
 
